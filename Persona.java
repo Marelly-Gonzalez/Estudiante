bd42c7a0da52493364bc880c8883475debc79781
Marelly-Gonzalez <maarhr2003@gmail.com>
2026-09-24 19:21:02 -0600

    Add Persona class with personal details and methods

---
+++ Persona.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+public class Persona {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private Date fechaNacimiento;
+
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+     public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+     public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String obtenerNombreCompleto() {
+        return nombre + "" + apellidoPaterno + "" + apellidoMaterno;
+    }
+}
